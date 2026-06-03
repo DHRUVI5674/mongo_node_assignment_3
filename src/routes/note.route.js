12872c62ede785698d242bd222188318919ff0f9
DHRUVI5674 <dhruvipatel5674@gmail.com>
2026-06-03 08:23:05 +0530

    feat: add put note by id endpoint (PUT /api/notes/:id)

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -7,12 +7,14 @@ const {
   createNote,
   multipleNotes,
   getAllNotes,
-  getNotesById
+  getNotesById,
+  UpdateById
 } = require('../controllers/note.controller.js');
 
 router.post('/', createNote);
 router.post('/multiple', multipleNotes);
 router.get('/', getAllNotes);
 router.get('/:id', getNotesById);
+router.put('/:id', UpdateById);
 
 module.exports = router;
