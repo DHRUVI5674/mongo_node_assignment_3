6c052cc79eb9ed3ea5f8c79d4fc9e7e4d92118ae
DHRUVI5674 <dhruvipatel5674@gmail.com>
2026-06-03 08:19:40 +0530

    feat: add get note by id endpoint (GET /api/notes/:id)

--- src/routes/note.route.js
+++ src/routes/note.route.js
@@ -6,11 +6,13 @@ console.log("note.route.js loaded");
 const {
   createNote,
   multipleNotes,
-  getAllNotes
+  getAllNotes,
+  getNotesById
 } = require('../controllers/note.controller.js');
 
 router.post('/', createNote);
 router.post('/multiple', multipleNotes);
 router.get('/', getAllNotes);
+router.get('/:id', getNotesById);
 
 module.exports = router;
